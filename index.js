5049121dabc41ed9d8538c6775292a1de8434203
Hamil1 <hamil151@hotmail.com>
2022-08-04 12:20:59 -0400

    2 files modified

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require('express');
 const app = express();
 const port = 3010;
 const path = require('path');
+const router = require('./routes');
 
-app.use(express.static('static'));
+// app.use(express.static('static'));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve('pages/index.html'));
-});
+// app.get('/', (req, res) => {
+//   res.sendFile(path.resolve('pages/index.html'));
+// });
+
+app.use(router);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,11 @@ import v1 from './v1/file.route';
 // TRANSFORMER FUNCTION API ROUTES
 
 //root route
-router.get('/', (req, res) => {
+router.get('/api', (req, res) => {
     res.send('Transformer API working...');
 });
 
 // V1 API routes
-router.use('/v1', v1);
+router.use('/api/v1', v1);
 
 export default router;
